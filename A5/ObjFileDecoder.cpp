cd27f05695ee285494fcee33c65127b430311e25
Jeremy White <j24white@uwaterloo.ca>
2017-11-22 23:22:28 -0500

    Optimize obj loading

--- A5/ObjFileDecoder.cpp
+++ A5/ObjFileDecoder.cpp
@@ -2,8 +2,6 @@
 using namespace glm;
 
 #include <fstream>
-#include <sstream>
-#include <iostream>
 #include <cstring>
 #include <cassert>
 #include <map>
@@ -11,57 +9,75 @@ using namespace std;
 
 #include "cs488-framework/Exception.hpp"
 
+#include "Utility.hpp"
+
 void parseMaterialLib(
 	const string& materialLibPath,
 	std::vector<MaterialData>& materials,
 	const map<string, std::size_t>& materialIndices
 ) {
+	vector<char> buffer;
+	Util::readFile(materialLibPath, buffer);
 
-	ifstream in(materialLibPath, std::ios::in);
-	in.exceptions(std::ifstream::badbit);
-
-	if (!in) {
-		stringstream errorMessage;
-		errorMessage << "Unable to open .mtl file " << materialLibPath
-					 << " within method parseMaterialLib" << endl;
-
-		throw Exception(errorMessage.str().c_str());
-	}
-
-	string currentLine;
 	MaterialData* currentMaterial = nullptr;
 
-	while (!in.eof()) {
-		try {
-			getline(in, currentLine);
-		}
-		catch (const ifstream::failure &e) {
-			in.close();
-			stringstream errorMessage;
-			errorMessage << "Error calling getline() -- " << e.what() << endl;
-			throw Exception(errorMessage.str());
-		}
-		if (currentLine.substr(0, 7) == "newmtl ") {
+	char* currentPos = buffer.data();
+
+	while (*currentPos != '\0') {
+		if (strncmp(currentPos, "newmtl ", 7) == 0) {
+			currentPos += 7;
+
 			string materialName;
-			istringstream s(currentLine.substr(7));
-			s >> materialName;
+
+			char* endPos = currentPos;
+			// advance until newline
+			while (!isspace(*endPos)) {
+				++endPos;
+			}
+
+			materialName = string(currentPos, endPos);
+			currentPos = endPos;
 
 			auto itr = materialIndices.find(materialName);
 			if (itr != materialIndices.end()) {
 				currentMaterial = &materials[itr->second];
 			}
 		}
-		else if (currentMaterial != nullptr && currentLine.substr(0, 7) == "map_Kd ") {
-			istringstream s(currentLine.substr(7));
-			s >> currentMaterial->diffuseMap;
+		else if (currentMaterial != nullptr && strncmp(currentPos, "map_Kd ", 7) == 0) {
+			currentPos += 7;
+
+			char* endPos = currentPos;
+			// advance until newline
+			while (!isspace(*endPos)) {
+				++endPos;
+			}
+
+			currentMaterial->diffuseMap = string(currentPos, endPos);
+			currentPos = endPos;
 		}
-		else if (currentMaterial != nullptr && currentLine.substr(0, 5) == "bump ") {
-			istringstream s(currentLine.substr(5));
-			s >> currentMaterial->bumpMap;
+		else if (currentMaterial != nullptr && strncmp(currentPos, "bump ", 5) == 0) {
+			currentPos += 5;
+
+			char* endPos = currentPos;
+			// advance until newline
+			while (!isspace(*endPos)) {
+				++endPos;
+			}
+
+			currentMaterial->bumpMap = string(currentPos, endPos);
+			currentPos = endPos;
+		}
+
+		// advance until newline
+		while (!iscntrl(*currentPos)) {
+			++currentPos;
+		}
+
+		// advance until next line with content
+		while (isspace(*currentPos)) {
+			++currentPos;
 		}
 	}
-
-	in.close();
 }
 
 //---------------------------------------------------------------------------------------
@@ -70,7 +86,7 @@ void ObjFileDecoder::decode(
 	std::string& objectName,
     std::vector<vec3>& positions,
     std::vector<vec3>& normals,
-    std::vector<vec2>& uvCoords,
+    std::vector<vec2>& uvs,
 	std::vector<FaceData>& faces,
 	std::vector<MaterialData>& materials
 ) {
@@ -78,99 +94,93 @@ void ObjFileDecoder::decode(
 	// Empty containers, and start fresh before inserting data from .obj file
 	positions.clear();
 	normals.clear();
-	uvCoords.clear();
+	uvs.clear();
 	faces.clear();
 	materials.clear();
 
-    ifstream in(objFilePath, std::ios::in);
-    in.exceptions(std::ifstream::badbit);
-
-    if (!in) {
-        stringstream errorMessage;
-        errorMessage << "Unable to open .obj file " << objFilePath
-            << " within method ObjFileDecoder::decode" << endl;
-
-        throw Exception(errorMessage.str().c_str());
-    }
+	vector<char> buffer;
+	Util::readFile(objFilePath, buffer);
 
 	// maps material names to indices of the groups vector
 	map<string, std::size_t> materialIndices;
 	string materialLibPath;
 
-    string currentLine;
+	char* currentPos = buffer.data();
 	unsigned short vn1, vn2, vn3, vt1, vt2, vt3;
 
 	objectName = "";
 
-    while (!in.eof()) {
-        try {
-            getline(in, currentLine);
-        } catch (const ifstream::failure &e) {
-            in.close();
-            stringstream errorMessage;
-            errorMessage << "Error calling getline() -- " << e.what() << endl;
-            throw Exception(errorMessage.str());
+    while (*currentPos != '\0') {
+		if (strncmp(currentPos, "v ", 2) == 0) {
+			currentPos += 2;
+
+			glm::vec3 vertex;
+
+			vertex.x = strtof(currentPos, &currentPos);
+			vertex.y = strtof(currentPos, &currentPos);
+			vertex.z = strtof(currentPos, &currentPos);
+
+            positions.push_back(move(vertex));
         }
-	    if (currentLine.substr(0, 2) == "o ") {
-		    // Get entire line excluding first 2 chars.
-		    istringstream s(currentLine.substr(2));
-		    s >> objectName;
-		}
-		else if (currentLine.substr(0, 2) == "v ") {
-            // Vertex data on this line.
-            // Get entire line excluding first 2 chars.
-            istringstream s(currentLine.substr(2));
-            glm::vec3 vertex;
-            s >> vertex.x;
-            s >> vertex.y;
-            s >> vertex.z;
-            positions.push_back(vertex);
+		else if (strncmp(currentPos, "vn ", 3) == 0) {
+			currentPos += 3;
+
+			vec3 normal;
+
+			normal.x = strtof(currentPos, &currentPos);
+			normal.y = strtof(currentPos, &currentPos);
+			normal.z = strtof(currentPos, &currentPos);
+
+			normals.push_back(move(normal));
         }
-		else if (currentLine.substr(0, 3) == "vn ") {
-            // Normal data on this line.
-            // Get entire line excluding first 2 chars.
-            istringstream s(currentLine.substr(2));
-            vec3 normal;
-            s >> normal.x;
-            s >> normal.y;
-            s >> normal.z;
-            normals.push_back(normal);
+		else if (strncmp(currentPos, "vt ", 3) == 0) {
+			currentPos += 3;
+
+			vec2 uv;
+
+			uv.s = strtof(currentPos, &currentPos);
+			uv.t = strtof(currentPos, &currentPos);
+
+			uv.t = 1.0f - uv.t;
+			uvs.push_back(move(uv));
         }
-		else if (currentLine.substr(0, 3) == "vt ") {
-            // Texture coordinate data on this line.
-            // Get entire line excluding first 2 chars.
-            istringstream s(currentLine.substr(2));
-            vec2 textureCoord;
-            s >> textureCoord.s;
-            s >> textureCoord.t;
-			textureCoord.t = 1.0f - textureCoord.t;
-            uvCoords.push_back(textureCoord);
-        }
-		else if (currentLine.substr(0, 2) == "f ") {
+		else if (strncmp(currentPos, "f ", 2) == 0) {
+			currentPos += 2;
+
             // Face index data on this line.
 
-            int index;
-
-            // sscanf will return the number of matched index values it found
-            // from the pattern.
-            int numberOfIndexMatches = sscanf(currentLine.c_str(), "f %d/%d/%d",
-                                              &index, &index, &index);
-
 			FaceData faceData;
+			int count = 0;
 
-            if (numberOfIndexMatches == 3) {
-                // Line contains indices of the pattern vertex/uv-cord/normal.
-                sscanf(currentLine.c_str(), "f %hu/%hu/%hu %hu/%hu/%hu %hu/%hu/%hu",
-                       &faceData.v1, &vt1, &vn1,
-					   &faceData.v2, &vt2, &vn2,
-					   &faceData.v3, &vt3, &vn3);
-            } else {
-                // Line contains indices of the pattern vertex//normal.
-                sscanf(currentLine.c_str(), "f %hu//%hu %hu//%hu %hu//%hu",
-					   &faceData.v1, &vn1,
-					   &faceData.v2, &vn2,
-					   &faceData.v3, &vn3);
-            }
+#define get static_cast<unsigned short>(strtoul(currentPos, &currentPos, 0))
+
+			faceData.v1 = get;
+			assert(*currentPos == '/');
+			++currentPos;
+			vt1 = get;
+			assert(*currentPos == '/');
+			++currentPos;
+			vn1 = get;
+
+			faceData.v2 = get;
+			assert(*currentPos == '/');
+			++currentPos;
+			vt2 = get;
+			assert(*currentPos == '/');
+			++currentPos;
+			vn2 = get;
+
+			faceData.v3 = get;
+			assert(*currentPos == '/');
+			++currentPos;
+			vt3 = get;
+			assert(*currentPos == '/');
+			++currentPos;
+			vn3 = get;
+
+#undef get
+
+			currentPos += count;
 
 			assert(
 				faceData.v1 == vt1 && faceData.v1 == vn1 &&
@@ -184,21 +194,56 @@ void ObjFileDecoder::decode(
 
 			faces.push_back(move(faceData));
 		}
-		else if (currentLine.substr(0, 7) == "mtllib ") {
-			istringstream s(currentLine.substr(7));
-			s >> materialLibPath;
-		}
-		else if (!materialLibPath.empty() && currentLine.substr(0, 7) == "usemtl ") {
-			istringstream s(currentLine.substr(7));
-			string materialName;
-			s >> materialName;
-			materialIndices.emplace(make_pair(materialName, materials.size()));
+		else if (strncmp(currentPos, "o ", 2) == 0) {
+			currentPos += 2;
 
+			char* endPos = currentPos;
+			// advance until newline
+			while (!isspace(*endPos)) {
+				++endPos;
+			}
+
+			objectName = string(currentPos, endPos);
+			currentPos = endPos;
+		}
+		else if (strncmp(currentPos, "mtllib ", 7) == 0) {
+			currentPos += 7;
+
+			char* endPos = currentPos;
+			// advance until newline
+			while (!isspace(*endPos)) {
+				++endPos;
+			}
+
+			materialLibPath = string(currentPos, endPos);
+			currentPos = endPos;
+		}
+		else if (!materialLibPath.empty() && strncmp(currentPos, "usemtl ", 7) == 0) {
+			currentPos += 7;
+
+			char* endPos = currentPos;
+			// advance until newline
+			while (!isspace(*endPos)) {
+				++endPos;
+			}
+
+			string materialName = string(currentPos, endPos);
+			materialIndices.emplace(make_pair(materialName, materials.size()));
 			materials.push_back(MaterialData{ faces.size(), "", "" });
+
+			currentPos = endPos;
+		}
+
+		// advance until newline
+		while (!iscntrl(*currentPos)) {
+			++currentPos;
+		}
+
+		// advance until next line with content
+		while (isspace(*currentPos)) {
+			++currentPos;
 		}
     }
-
-    in.close();
 
 	if (objectName.compare("") == 0) {
 		// No 'o' object name tag defined in .obj file, so use the file name
